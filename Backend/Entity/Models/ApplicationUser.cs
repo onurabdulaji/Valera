3557a157235812b0aa864494812308d21e32ea7c
Onur Abdulaji <onurabdulaji@gmail.com>
2024-11-20 18:54:04 +0100

    Identity ve EF Core paketleri eklendi.
    AppRole ve AppUser entitileri eklendi.

---
+++ Backend/Entity/Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Models
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+    }
+}
